bb74db77780b4dce56246eabfad6e3804e972a7a
Juancho1s <113451623+Juancho1s@users.noreply.github.com>
2022-10-22 10:42:45 -0500

    comit

--- Practice2/Graphs/Methods.cs
+++ Practice2/Graphs/Methods.cs
@@ -28,49 +28,8 @@ namespace Graphs
 
         public void addToTheGraphDirection(int newNode, int nodeConection, int weight)
         {
-            foreach (Node node in nodesList)
-            {
-                if (node.data == nodeConection)
-                {
-                    for (int i = 0; i < nodesList.Count; i++)
-                    {
-                        if (nodesList[i].data == newNode)
-                        {
-                            for (int j = 0; j < nodesList[i].nodesConectios.Count; j++)
-                            {
-                                if (nodesList[i].nodesConectios[j].data)
-                                {
-
-                                }
-                            }
-                            node.nodesConectios.Add();
-                            edges.Add(new int[] { nodeConection, newNode, weight });
-                            return;
-                        }
-                    }                    
-                }
-            }
-        }
-
-        public void addToTheGraphNoDirection(int newNode, int nodeConection, int weight)
-        {
-            foreach (Node node in nodesList)
-            {
-                if (node.data == nodeConection)
-                {
-                    foreach (Node search in nodesList)
-                    {
-                        if (search.data == newNode)
-                        {
-                            node.nodesConectios.Add(search);
-                            search.nodesConectios.Add(node);
-                            edges.Add(new int[] { nodeConection, newNode, weight });
-                            return;
-                        }
-                    }
-                }
-            }
-        }
+            
+        }        
 
         //////////
 
