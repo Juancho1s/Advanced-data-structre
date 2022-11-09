b3baece92d4f87d8365a761df8552acbbc20e12a
Juancho1s <113451623+Juancho1s@users.noreply.github.com>
2022-11-08 23:43:29 -0600

    commiting all

---
+++ Practice2/GraphicInterface/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphicInterface.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hola, Que hace? Estudiando o que?";
+    }
+}
---
+++ Practice2/GraphicInterface/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphicInterface.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
